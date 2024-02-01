1147f0f50c4d0720f5d3f7339144a7363873e2d3
pietroggs <30303680+pietroggs@users.noreply.github.com>
2024-02-01 16:58:58 -0300

    Save

---
+++ course/screens/100/tela.js
@@ -0,0 +1,25 @@
+//Para abrir em outro card só trocar o t
+let t = 1, 
+telas = 0
+
+function init() {
+    telas = document.querySelectorAll('section')
+    telas.forEach((e, index) => {
+        if (index == t)
+            return
+        e.style.display = 'none'
+    })
+}
+
+function nextCard() {
+    if ((t + 1) == telas.length){
+        console.log('aqui acaba')
+    }
+    telas[t].style.display = 'none'
+    t++
+    telas[t].style.display = 'block'
+}
+
+function closeBalao(e){
+    e.parentElement.parentElement.removeChild(e.parentElement)
+}
