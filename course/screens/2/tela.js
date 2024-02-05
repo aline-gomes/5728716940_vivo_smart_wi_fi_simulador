063731005c11d26475b233866c27766409b8b5af
aline-gomes <64175641+aline-gomes@users.noreply.github.com>
2024-02-05 19:05:45 -0300

    commit ajustes módulo 1

--- course/screens/2/tela.js
+++ course/screens/2/tela.js
@@ -1,61 +1,43 @@
-var t = 0, telas = 17
-var btn = document.querySelector('.destaque');
+//Para abrir em outro card só trocar o t
+var t = 0, telas = 0
 var elements = document.querySelectorAll('.anim');
-var destaque = document.querySelector('.destaque');
 var user_number = document.querySelector(".user_number");
 var mascara_balao = document.querySelector(".mascara_balao");
 
 function init() {
     Anim();
     user_number.style.display = 'none'
-    document.querySelector('section').style.display = 'none'
+    telas = document.querySelectorAll('section')
+    telas.forEach((e, index) => {
+        if (index == t)
+            return
+        e.style.display = 'none'
+    })
 }
 
 function nextCard() {
-    if (t == telas) {
-        document.querySelector('.prints_modulo_1').id = `container_print_${t}`
-
-        mascara_balao.style.cssText = ''
-        destaque.style.cssText = 'display: none;'
-        document.querySelector('.balao p').innerHTML = '<strong>Pronto!</strong><br> Você está na tela inicial do App Vivo Smart Wi-Fi.'
-
-        document.querySelector('.seta').style.display = 'none'
-        document.querySelector('.hole').style.display = 'none'
-        document.querySelector('.balao .fechar').onclick = _ => { screen.goto(1); };
-
+    if ((t + 1) == telas.length) {
+        screen.goto(1)
         console.log('aqui acaba')
         return;
     }
 
-    btn.id = `d${t}`
+    telas[t].style.display = 'none'
     t++
+    telas[t].style.display = 'block'
 
-    if (t == 6 || t == 13 || t == 16) {
-        btn.parentNode.style.cssText = `background-image: url(./screens/2/img/print_${t}.gif);`
+    gsap.from(telas[t], .5, { autoAlpha: 0, onComplete: function () { gsap.set(telas[t], { clearProps: true }); } });
+
+    if (t == 1 || t == 7 || t == 16 || t == 19) {
         setTimeout(() => { nextCard(); }, 3000);
         return;
     }
 
-    if (t == 7 || t == 9 || t == 11) showInputs();
-    btn.parentNode.style.cssText = `background-image: url(./screens/2/img/print_${t}.png);`
-}
-
-var load_fake = document.querySelector('.load_fake');
-
-const showLoadFake = (element) => {
-    load_fake.style.cssText = 'display: block;'
-    element.parentNode.style.cssText = 'background-image: none;'
-
-    setTimeout(() => {
-        load_fake.style.cssText = 'display: none;'
-        document.querySelector('#btn_vivinho').style.display = 'none'
-        nextCard()
-    }, 3000);
+    if (t == 8 || t == 11 || t == 13) showInputs();
 }
 
 user_number.addEventListener("focus", e => {
     e.target.value = "";
-    destaque.classList.remove('pulse')
 })
 
 var inputResp = ['00000000000', '000000', '123456'];
@@ -65,7 +47,6 @@ function onInput(index, max_length) {
     user_number.value = number;
 
     let result = /^\d+$/.test(number);
-    destaque.style.cssText = 'border: none;'
 
     if (!result || number !== inputResp[index] && number.length == max_length) {
         document.querySelector('.feedback').style.display = 'flex'
@@ -73,76 +54,72 @@ function onInput(index, max_length) {
     } else {
         if (number === inputResp[index]) {
             console.log('deu certo!')
-            if (index == 0) btn.parentNode.style.cssText = 'background-image: url(./screens/2/img/print_7_2.png);'
             user_number.classList.add('lock')
-            nextDestaque(index)
+
+            if (t == 11) {
+                setTimeout(() => { hideInput(); nextCard(); }, 200);
+                return;
+            }
+            nextCard();
         }
-    }
-}
-
-var nextDestaque = (index) => {
-    if (index == 1) {
-        nextCard();
-        user_number.style.cssText = 'display: none;'
-        destaque.className = 'destaque abs pulse'
-        destaque.style.cssText = ''
-        return;
-    }
-
-    var divBtn = document.createElement('div');
-    (t == 7) ? divBtn.id = 'd6_2' : divBtn.id = 'd10_2';
-    divBtn.className = 'destaque abs pulse'
-    document.querySelector('.prints_modulo_1').appendChild(divBtn);
-
-    divBtn.onclick = _ => {
-        user_number.style.cssText = 'display: none;'
-        destaque.className = 'destaque abs pulse'
-        destaque.style.cssText = ''
-        divBtn.remove()
-        nextCard()
     }
 }
 
 var showInputs = _ => {
     let pop_text = document.querySelector('.balao p');
 
-    user_number.classList.remove('lock')
-    user_number.style.cssText = 'display: block;'
+    user_number.value = 'Número';
+    user_number.className = 'user_number abs lock'
 
-    destaque.classList.add('lock')
-    destaque.classList.remove('pulse')
     mascara_balao.style.cssText = ''
     user_number.oninput = () => { };
 
     switch (true) {
-        case (t == 7):
+        case (t == 8):
+            user_number.style.cssText = 'display: block; border: 5px dashed #609;'
             pop_text.innerHTML = '<p>Preencha o campo com<br>o número 00000000000.</p>'
             user_number.oninput = () => { onInput(0, 11); };
             break;
 
-        case (t == 9):
-            user_number.value = ''
-            user_number.style.cssText = 'display: block; top: 26%; left: 51.3%; width: 27rem; padding: 0.5rem; letter-spacing: 3.8rem;'
+        case (t == 11):
+            user_number.value = '';
+            user_number.style.cssText = 'display: block; border: 5px dashed #609; top: 26%; left: 51.3%; width: 27rem; padding: 0.5rem; letter-spacing: 3.8rem;'
             pop_text.innerHTML = '<p>Preencha o campo com<br>o número 000000.</p>'
             user_number.oninput = () => { onInput(1, 6); };
             break;
 
-        case (t == 11):
-            user_number.value = ''
-            user_number.style.cssText = 'display: block; top: 20%; padding: 0;'
+        case (t == 13):
+            user_number.style.cssText = 'display: block; border: 5px dashed #609; top: 20%; padding: 0;'
             pop_text.innerHTML = '<p>Preencha o campo com<br>o número 123456.</p>'
             user_number.oninput = () => { onInput(2, 6); };
             break;
     }
 }
 
-function closeBalao(element) {
-    destaque.classList.add('pulse')
-    element.parentElement.parentElement.style.cssText = 'display: none;'
+function hideInput() {
+    user_number.style.cssText = 'display: none;'
 }
 
 function closeFeedBack(element) {
     element.parentElement.style.cssText = 'display: none;'
+}
+
+function openLastMask() {
+    mascara_balao.style.display = 'block'
+
+    document.querySelector('.seta').style.display = 'none'
+    document.querySelector('.hole').style.display = 'none'
+    document.querySelector('.balao p').innerHTML = '<strong>Pronto!</strong><br> Você está na tela inicial do App Vivo Smart Wi-Fi.'
+
+    document.querySelector('.balao .fechar').onclick = _ => { nextCard(); };
+}
+
+function closeBalao(element) {
+    if (t == 8 || t == 11 || t == 13) {
+        user_number.classList.remove('lock');
+        user_number.style.border = 'none'
+    }
+    element.parentElement.parentElement.style.cssText = 'display: none;'
 }
 
 // ANIMATION
