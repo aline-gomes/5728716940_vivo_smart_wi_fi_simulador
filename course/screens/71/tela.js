2bbc20877a452594e37274c0919dce27635a58af
aline-gomes <64175641+aline-gomes@users.noreply.github.com>
2024-02-07 08:36:04 -0300

    commit capitalize

--- course/screens/71/tela.js
+++ course/screens/71/tela.js
@@ -1,83 +1,85 @@
 function init() {
 
-    let validString = "Joanna";
+  let validString = "Joanna";
 
-    // refers
-    let mascara = document.querySelector(".mascara_balao");
-    let balao = document.querySelector(".balao.b0");
-    let btFecharBalao = document.querySelector(".balao button.fechar");
+  // refers
+  let mascara = document.querySelector(".mascara_balao");
+  let balao = document.querySelector(".balao.b0");
+  let btFecharBalao = document.querySelector(".balao button.fechar");
 
-    let destaques = document.querySelectorAll(".destaque");
+  let destaques = document.querySelectorAll(".destaque");
 
-    let inputField = document.querySelector("input.nome");
+  let inputField = document.querySelector("input.nome");
 
-    let feedNeg = document.querySelector(".feedback");
-    let btFecharFeed = document.querySelector(".feedback button.fechar");
-    
-    // on first focus...
-    inputField.addEventListener("focus", e => {
-      e.target.value = "";
-      destaques[0].style.display = "none";
-    })
-    
-    // on leaving field...
-    inputField.addEventListener("blur", e => {
-      e.target.value = e.target.value === "" ? "Nome" : e.target.value;
-    })
+  let feedNeg = document.querySelector(".feedback");
+  let btFecharFeed = document.querySelector(".feedback button.fechar");
 
-    // on typing...
-    inputField.addEventListener("input", e => {
-      if (e.target.value.length >= validString.length) {
-        // enable pulse for avatar sel.
-        destaques[1].style.display = "block";
-      } 
-    })
+  // on first focus...
+  inputField.addEventListener("focus", e => {
+    e.target.value = "";
+    destaques[0].style.display = "none";
+  })
 
-    // balloon events
-    let showDestaque = (dest) => {
-      destaques[dest].style.display = "block";
+  // on leaving field...
+  inputField.addEventListener("blur", e => {
+    e.target.value = e.target.value === "" ? "Nome" : e.target.value;
+  })
+
+  // on typing...
+  inputField.addEventListener("input", e => {
+    if (e.target.value.length >= validString.length) {
+      // enable pulse for avatar sel.
+      destaques[1].style.display = "block";
     }
+  })
 
-    // close balloon event
-    let handleCloseBalloon = (e) => {
-      e.target.parentNode.style.display = "none";
-      mascara.style.display = "none";
+  // balloon events
+  let showDestaque = (dest) => {
+    destaques[dest].style.display = "block";
+  }
 
-      showDestaque(0);
+  // close balloon event
+  let handleCloseBalloon = (e) => {
+    e.target.parentNode.style.display = "none";
+    mascara.style.display = "none";
+
+    showDestaque(0);
+  }
+  btFecharBalao.addEventListener("click", handleCloseBalloon);
+
+  // validate string, return bool
+  let checkInput = (inp) => {
+    inp.value = inp.value.replace(/^./, str => str.toUpperCase())
+
+    if (inp.value === validString) {
+      return true;
+    } else {
+      return false;
     }
-    btFecharBalao.addEventListener("click", handleCloseBalloon);
+  }
 
-    // validate string, return bool
-    let checkInput = (inp) => {
-      if(inp.value === validString) {
-        return true;
-      } else {
-        return false;
-      }
+  // destaque events
+  let handleAvancar = (e) => {
+    if (checkInput(inputField)) {
+      screen.next();
+    } else {
+      // Incorrect feedback! Show feed =>
+      feedNeg.style.display = "flex";
     }
+  }
+  destaques[1].addEventListener("click", handleAvancar);
 
-    // destaque events
-    let handleAvancar = (e) => {
-      if (checkInput(inputField)) {
-        screen.next();
-      } else {
-        // Incorrect feedback! Show feed =>
-        feedNeg.style.display = "flex";
-      }
-    }
-    destaques[1].addEventListener("click", handleAvancar);
+  // feedback events
+  let handleCloseFeed = (e) => {
+    // Close feed, clean input + hide destaque[1] 
+    e.target.parentNode.style.display = "none";
+    inputField.value = "";
+    destaques[1].style.display = "none";
+  }
+  btFecharFeed.addEventListener("click", handleCloseFeed);
 
-    // feedback events
-    let handleCloseFeed = (e) => {
-      // Close feed, clean input + hide destaque[1] 
-      e.target.parentNode.style.display = "none";
-      inputField.value = "";
-      destaques[1].style.display = "none";
-    }
-    btFecharFeed.addEventListener("click", handleCloseFeed);
-    
-    // Animation
-    let tl = new gsap.timeline();
-    tl.fromTo(mascara, .8, { opacity: 0 }, { opacity: 1, delay: .5 });
-    tl.fromTo(balao, .8, { opacity: 0 }, { opacity: 1 });
+  // Animation
+  let tl = new gsap.timeline();
+  tl.fromTo(mascara, .8, { opacity: 0 }, { opacity: 1, delay: .5 });
+  tl.fromTo(balao, .8, { opacity: 0 }, { opacity: 1 });
 }
